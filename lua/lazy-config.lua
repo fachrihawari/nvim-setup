f477838dcacebc8059798d7cbadfd7e03c558591
Fachri Hawari <fachri.hawari@gmail.com>
2024-10-19 07:34:26 +0700

    remove plugins

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -1,3 +1,4 @@
+-- setup lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -11,5 +12,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- setup lazy plugins
+require('lazy').setup {
+  { -- VSCode Light Theme
+    'Mofiqul/vscode.nvim'
+  },
+  { -- Statusline for Bottom status bar
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+}
 
-require('lazy').setup('plugins')
