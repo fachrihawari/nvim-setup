e0ef33268961272bf815265f03361a2208b59f17
Fachri Hawari <fachri.hawari@gmail.com>
2024-10-23 11:51:19 +0700

    tidy up mappings and add terminal

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@ require 'keymaps' -- setup keymaps
 require 'options' -- default vim configuration
 require 'lazy-config' -- setup lazy package manager
 require 'vscode-theme-config' -- setup vscode theme
+require 'which-key-config' -- setup key bindings
 require 'lualine-config' -- setup lualine for status bar
+require 'bufferline-config' -- setup tab bar
 require 'nvim-tree-config' -- setup nvim-tree for file explorer
 require 'telescope-config' -- setup telescope for search
-require 'which-key-config' -- setup key bindings
--- lua/which-key-config.lua
+++ lua/which-key-config.lua
@@ -3,34 +3,30 @@ if not status_ok then
   return
 end
 
-
-local opts = {
-    mode = "n", -- NORMAL mode
-    prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
-    noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
-}
-
 local mappings = {
   -- Navigation
-  { "<leader>e", "<wincmd>w<cr>", desc = "Switch window", nowait = true, remap = false },
+  { "<leader>e", "<cmd>wincmd w<cr>", desc = "Switch window", nowait = true, remap = false },
+  { "<tab>", "<cmd>bnext<cr>", desc = "Next buffer", nowait = true, remap = false },
 
   -- Telescope for search
   { "<leader>f", group = "File Search", nowait = true, remap = false },
   { "<leader>fc", "<cmd>Telescope colorscheme<cr>", desc = "Colorscheme", nowait = true, remap = false },
-  { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = "Find files", nowait = true, remap = false },
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files", nowait = true, remap = false },
   { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File", nowait = true, remap = false },
   { "<leader>ft", "<cmd>Telescope live_grep <cr>", desc = "Find Text Pattern In All Files", nowait = true, remap = false },
-  
+
   -- Lazy Package Manager
   { "<leader>p", "<cmd>Lazy<cr>", desc = "Plugin Manager", nowait = true, remap = false },
-  
+
   -- Action
-  { "<leader>q", "<cmd>wqall!<CR>", desc = "Quit", nowait = true, remap = false },
-  { "<leader>s", "<cmd>w!<CR>", desc = "Save", nowait = true, remap = false },
+  { "<leader>q", "<cmd>wqall!<cr>", desc = "Quit", nowait = true, remap = false },
+  { "<leader>s", "<cmd>w!<cr>", desc = "Save", nowait = true, remap = false },
   { "<leader>w", "<cmd>bdelete | bprevious<cr>", desc = "Kill Buffer", nowait = true, remap = false },
+
+  -- Terminal
+  { "<leader>t", group = "Terminal", nowait = true, remap = false },
+  { "<leader>tt", "<cmd>split | terminal<cr>", desc = "Terminal horizontal", nowait = true, remap = false },
+  { "<leader>ty", "<cmd>vsplit | terminal<cr>", desc = "Terminal vertical", nowait = true, remap = false },
 }
 
 which_key.add(mappings)
