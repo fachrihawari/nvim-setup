04ca82c7667770ea9c50c7cc0eccb730085685ae
Fachri Hawari <fachri.hawari@gmail.com>
2024-10-30 11:47:29 +0700

    add show hide sidebar

--- lua/which-key-config.lua
+++ lua/which-key-config.lua
@@ -5,6 +5,7 @@ end
 
 local mappings = {
   -- Navigation
+  { "<leader>b", "<cmd>NvimTreeToggle<cr>", desc = "Switch window", nowait = true, remap = false },
   { "<leader>e", "<cmd>wincmd w<cr>", desc = "Switch window", nowait = true, remap = false },
   { "<tab>", "<cmd>bnext<cr>", desc = "Next buffer", nowait = true, remap = false },
 
